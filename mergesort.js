efcbc87ae515536e6c8716fd3b1690326a40beed
Keisuke Kido <kskkido@gmail.com>
2017-03-02 15:01:50 -0500

    ballin

--- mergesort.js
+++ mergesort.js
@@ -5,18 +5,27 @@ function split (arr) {
 }
 
 function merge(arr){
-	if (arr.length ===1){
+	var outputArr = []
+	if (arr.length === 1){
 		return arr;
 	}
 	else {
 		var splitArray = split(arr);
 		var firstVal = merge(splitArray[0]);
 		var secondVal = merge(splitArray[1]);
-		if (firstVal>secondVal){
-			return secondVal.concat(firstVal);
+			var tempArr = [];
+			var tempFirstArr = firstVal
+			var tempSecondArr = secondVal
+			var tempLength = tempFirstArr.length + tempSecondArr.length
+			for (var i = 0; i < tempLength;i++) {
+				if (tempSecondArr[0] == undefined || tempFirstArr[0] < tempSecondArr[0]){
+					tempArr.push(tempFirstArr.shift())
+				}
+				else if (tempFirstArr[0] == undefined || tempFirstArr[0] > tempSecondArr[0]) {
+					tempArr.push(tempSecondArr.shift())
+				} 
+
+			}
+			return tempArr
 		}
-		else{
-			return firstVal.concat(secondVal);
-		}
-	}
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,10 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    // your code here 
+    expect(split([1,3,2,5])).toEqual([[1,3],[2,5]]);
   });
+});
+describe('Merge Sort', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    expect( merge([6,1,3,5,0,-1]) ).toEqual([-1,0,1,3,5,6]);
+   });
 });
