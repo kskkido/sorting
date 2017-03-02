3670c986a0fe07f3b6479d900d3e1fbe3e3b4e1e
Keisuke Kido <kskkido@gmail.com>
2017-03-02 15:05:32 -0500

    trying to git

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -8,3 +8,4 @@ describe('Bubble Sort', function(){
     expect( bubbleSort([1,4,2,3,-5,32,333,65,7]) ).toEqual([-5,1,2,3,4,7,32,65,333]);
   });
 });
+
--- bubblsesort.js
+++ bubblsesort.js
@@ -15,8 +15,3 @@ function bubbleSort (arr) {
 	return res
 }
 
-function split (arr) {
-	var res1= arr.slice(0,Math.floor(arr.length/2))
-	var res2 = arr.slice(Math.floor(arr.length/2))
-	return [res1,res2];
-}
